f241eae318a9893bf50fe6617466179cfb2d82e7
Jody-Dixon <jodymdixon@gmail.com>
2020-06-30 18:51:15 -0400

    Got most of the html and css complete image is not center

---
+++ main.js
@@ -0,0 +1,8 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelector('.slide');
+
+function nextSlide()
+
+
+next.addEventListener('click', nextSlide)
