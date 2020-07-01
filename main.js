dbc338e475c3de2fd7d75832c7762376a4fa746d
Jody-Dixon <jodymdixon@gmail.com>
2020-07-01 14:29:24 -0400

    Functionality finalized - need to tweek css

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
-const slides = document.querySelector('.slide');
+const slides = document.querySelectorAll('.slide');
 
-function nextSlide()
+let index = 0;
+
+function display() {
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';
+}
+
+function nextSlide() {
+    index ++;
+    if(index > slides.length - 1){
+        index = 0;
+    }
+    display();
+}
+
+function prevSlide() {
+    index --;
+    if(index < 0){
+        index = slides.length - 1;
+    }
+    display();
+}
 
 
+
+prev.addEventListener('click', prevSlide)
 next.addEventListener('click', nextSlide)
